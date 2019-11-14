d178b85332a09442614b4249c4eac665e1047983
Aljaed Lui Ereno <alereno@hawaii.edu>
2019-11-13 20:21:41 -1000

    Footer and Landing page changed

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center'>
-          <Grid.Column>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
-          </Grid.Column>
-        </Grid>
+        <div>
+          <Grid verticalAlign='middle' textAlign='center'
+                style={{
+                  height: 400,
+                  backgroundSize: 'cover',
+                  backgroundImage: `url(${'images/landingmain.jpg'})`,
+                }}>
+            <Grid.Column>
+              <h1 style={{
+                color: 'white',
+              }}>Beyond the Library</h1>
+              <p style={{
+                color: 'white',
+              }}
+              >Hit the books in these hit places</p>
+            </Grid.Column>
+          </Grid>
+        </div>
     );
   }
 }
