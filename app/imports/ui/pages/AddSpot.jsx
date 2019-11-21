249d3ea861a05b29f00d8cf15a223f585e8167d1
Yuhan Jiang <jiangyuh@hawaii.edu>
2019-11-20 14:24:39 -1000

    add spot

--- app/imports/ui/pages/AddSpot.jsx
+++ app/imports/ui/pages/AddSpot.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Container, Grid, Image, Header, Button } from 'semantic-ui-react';
+import { Card, Container, Grid, Image, Header, Button, Label, Icon } from 'semantic-ui-react';
 
 class AddSpot extends React.Component {
   render() {
@@ -11,7 +11,7 @@ class AddSpot extends React.Component {
     return (
         <Container>
           <Card.Group>
-            <Card fluid color='red' height='200px'>
+            <Card fluid color='orange' height='200px'>
               <Card.Content>
               <Grid container>
                 <Grid.Column width={3}>
@@ -19,22 +19,49 @@ class AddSpot extends React.Component {
                       src="https://cnet3.cbsistatic.com/img/pXZLKD-YlCz9n12JlGhovk4YCdo=/1092x0/2014/03/31/1497f64b-bf2d-11e3-bddd-d4ae52e62bcc/bliss_1.jpg"/>
                 </Grid.Column>
                 <Grid.Column width={9}>
-                  <Header as='h5'>Location: Somewhere in the earth</Header>
-                  <Header as='h5'>Description: Everyone knows this spot.</Header>
+                  <Header as='h4'>Location: Somewhere in the earth</Header>
+                  <Header as='h4'>Description: Everyone knows this spot.</Header>
                 </Grid.Column>
-                <Grid.Column width={3}>
-                  <Button.Group>
-                    <Button positive>Edit</Button>
-                    <Button.Or />
-                    <Button negative>Discard</Button>
-
-                  </Button.Group>
+                <Grid.Column width={3} rows='2' stretched>
+                  <Grid.Row>
+                    <Label size='large'>
+                      <Icon name='circle outline' color='orange' /> Pending
+                    </Label>
+                  </Grid.Row>
+                  <Grid.Row>
+                    <Button.Group>
+                      <Button positive>Edit</Button>
+                      <Button.Or />
+                      <Button negative>Discard</Button>
+                    </Button.Group>
+                  </Grid.Row>
                 </Grid.Column>
               </Grid>
               </Card.Content>
             </Card>
-            <Card fluid color='orange' header='Option 2'/>
-            <Card fluid color='yellow' header='Option 3'/>
+
+
+            <Card fluid color='green' height='200px'>
+              <Card.Content>
+                <Grid container>
+                  <Grid.Column width={3}>
+                    <Image verticalAlign='middle' fluid rounded
+                           src="https://cnet3.cbsistatic.com/img/pXZLKD-YlCz9n12JlGhovk4YCdo=/1092x0/2014/03/31/1497f64b-bf2d-11e3-bddd-d4ae52e62bcc/bliss_1.jpg"/>
+                  </Grid.Column>
+                  <Grid.Column width={9}>
+                    <Header as='h4'>Location: Somewhere in the earth</Header>
+                    <Header as='h4'>Description: Everyone knows this spot.</Header>
+                  </Grid.Column>
+                  <Grid.Column width={3} rows='2' stretched>
+                    <Grid.Row>
+                      <Label size='large'>
+                        <Icon name='circle outline' color='green' /> Approved
+                      </Label>
+                    </Grid.Row>
+                  </Grid.Column>
+                </Grid>
+              </Card.Content>
+            </Card>
           </Card.Group>
         </Container>
 
