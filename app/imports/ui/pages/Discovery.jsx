d1497589f53543f1b82403adfd1057fccafcd3a1
Andrew Salazar <35311333+ASalazar46@users.noreply.github.com>
2019-12-04 20:19:16 -1000

    Have the filter working, might need to touch it up a bit later on

--- app/imports/ui/pages/Discovery.jsx
+++ app/imports/ui/pages/Discovery.jsx
@@ -1,14 +1,74 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Container, Header, Form, Card } from 'semantic-ui-react';
+import { Container, Header, Dropdown, Menu, Card, Button } from 'semantic-ui-react';
 import SpotCard from '/imports/ui/components/SpotCard.jsx';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { _ } from 'meteor/underscore';
 import { Spots } from '../../api/spot/Spots.js';
 
+/** {this.props.spots.map((spot, index) => <SpotCard key={index} spot={spot}/>)} */
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class Discovery extends React.Component {
+
+  state = {
+    spots: [],
+    searchBy: 'Major',
+    currentValue: [],
+  };
+
+  constructor(props) {
+    super(props);
+    this.handleFilterChange = this.handleFilterChange.bind(this);
+    this.handleGeneralChange = this.handleGeneralChange.bind(this);
+    this.createOptions = this.createOptions.bind(this);
+    this.setSearchBy = this.setSearchBy.bind(this);
+    this.handleAddition = this.handleAddition.bind(this);
+    this.onClickClear = this.onClickClear.bind(this);
+  }
+
+  createOptions() {
+    this.searchBy = [
+      { key: 1, value: 'major', text: 'Major' },
+      { key: 2, value: 'environment', text: 'Environment' },
+      { key: 3, value: 'time', text: 'Time' },
+    ];
+    // eslint-disable-next-line no-restricted-syntax
+    for (const category of this.searchBy) {
+      const list = _.uniq(_.pluck(this.props.spots, category.value).flatten()).sort();
+      this[category.value] = list.map((value, index) => ({ key: index, value: value, text: value }));
+    }
+  }
+
+  returnSpot(spotId) {
+    return Spots.findOne({ _id: spotId });
+  }
+
+  setSearchBy(event, data) {
+    this.setState({ searchBy: data.value });
+  }
+
+  handleAddition(e, { value }, category) {
+    const spots = this.props.spots.filter((x) => (x[category].toUpperCase().indexOf(value.toUpperCase()) !== -1));
+    this.setState({ spots: spots });
+  }
+
+  handleGeneralChange(event, data, category) {
+    const spots = this.props.spots.filter((x) => (x[category].toUpperCase().indexOf(data.value.toUpperCase()) !== -1));
+    this.setState({ spots: spots });
+  }
+
+  handleFilterChange(event, data) {
+    /* eslint-disable-next-line */
+    const spots = this.props.spots.filter((x) => _.intersection(x.spots, data.value).length === data.value.length);
+    this.setState({ spots: spots });
+    this.setState({ currentValue: data.value });
+  }
+
+  onClickClear() {
+    this.setState({ spots: [] });
+    this.setState({ currentValue: [] });
+  }
 
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
@@ -17,39 +77,36 @@ class Discovery extends React.Component {
 
   /** Render the page once subscriptions have been received. */
   renderPage() {
+    if (this.spots === undefined) {
+      this.createOptions();
+    }
+
     return (
         <Container>
           <Header as="h2" textAlign="center" inverted>Discover New Spots</Header>
-          <Form>
-            <Form.Group widths='equal'>
-              <Form.Input fluid label='Name' placeholder='Name'/>
-              <Form.Select
-                  fluid
-                  label='Major'
-                  options={[{ key: 'Computer Science', text: 'CS' }, { key: 'Computer Engineering', text: 'CENG' },
-                    { key: 'Music', text: 'Music' }, { key: 'Open for everyone', text: 'Open for everyone' }]}
-                  placeholder='Major'
-              />
-              <Form.Select
-                  fluid
-                  label='Location'
-                  options={[{ key: 'onc', text: 'On Campus' }, { key: 'offc', text: 'Off Campus' }]}
-                  placeholder='Location'
-              />
-              <Form.Select
-                  fluid
-                  label='Environment'
-                  options={[{ key: 'i', text: 'Indoor' }, { key: 'o', text: 'Outdoor' }]}
-                  placeholder='Environment'
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Button>Apply Filter</Form.Button>
-              <Form.Button>Add a New Study Spot</Form.Button>
-            </Form.Group>
-          </Form>
+          <Menu>
+            <Dropdown selection defaultValue='Major' options={this.searchBy}
+                      onChange={(e, data) => this.setSearchBy(e, data)}/>
+            {this.state.searchBy === 'Major' ? (
+                <Dropdown placeholder='Filter Values' fluid multiple search selection
+                          options={this.spots} value={this.state.currentValue} icon='search'
+                          onChange={(event, data) => this.handleFilterChange(event, data)}
+                />
+            ) : (
+                <Dropdown placeholder={'Choose a Filter'} deburr fluid search selection
+                          options={this[this.state.searchBy]} icon='search' allowAdditions additionLabel=''
+                          onChange={(event, data) => this.handleGeneralChange(event, data, this.state.searchBy)}
+                          onAddItem={(e, data) => this.handleAddition(e, data, this.state.searchBy)}
+                />
+            )}
+            <Button negative onClick={this.onClickClear}>Clear</Button>
+          </Menu>
           <Card.Group>
-            {this.props.spots.map((spot, index) => <SpotCard key={index} spot={spot}/>)}
+            {this.state.spots.length === 0 ? (
+                this.props.spots.map((spot, index) => <SpotCard key={index} spot={spot}/>)
+            ) : (
+                this.state.spots.map((spot, index) => <SpotCard key={index} spot={this.returnSpot(spot._id)}/>))
+            }
           </Card.Group>
         </Container>
     );
