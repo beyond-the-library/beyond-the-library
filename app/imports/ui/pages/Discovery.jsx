84b2764e1088e95484d9807a11bf2677ead40442
Andrew Salazar <35311333+ASalazar46@users.noreply.github.com>
2019-12-04 20:29:48 -1000

    Removed merge conflict with issue 20

--- app/imports/ui/pages/Discovery.jsx
+++ app/imports/ui/pages/Discovery.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Container, Header, Form, Card } from 'semantic-ui-react';
+import { Container, Header, Card } from 'semantic-ui-react';
 import SpotCard from '/imports/ui/components/SpotCard.jsx';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
-import { _ } from 'meteor/underscore';
 import { Spots } from '../../api/spot/Spots.js';
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
@@ -20,34 +19,6 @@ class Discovery extends React.Component {
     return (
         <Container>
           <Header as="h2" textAlign="center" inverted>Discover New Spots</Header>
-          <Form>
-            <Form.Group widths='equal'>
-              <Form.Input fluid label='Name' placeholder='Name'/>
-              <Form.Select
-                  fluid
-                  label='Major'
-                  options={[{ key: 'Computer Science', text: 'CS' }, { key: 'Computer Engineering', text: 'CENG' },
-                    { key: 'Music', text: 'Music' }, { key: 'Open for everyone', text: 'Open for everyone' }]}
-                  placeholder='Major'
-              />
-              <Form.Select
-                  fluid
-                  label='Location'
-                  options={[{ key: 'onc', text: 'On Campus' }, { key: 'offc', text: 'Off Campus' }]}
-                  placeholder='Location'
-              />
-              <Form.Select
-                  fluid
-                  label='Environment'
-                  options={[{ key: 'i', text: 'Indoor' }, { key: 'o', text: 'Outdoor' }]}
-                  placeholder='Environment'
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Button>Apply Filter</Form.Button>
-              <Form.Button>Add a New Study Spot</Form.Button>
-            </Form.Group>
-          </Form>
           <Card.Group centered>
             {this.props.spots.map((spot, index) => <SpotCard key={index} spot={spot}/>)}
           </Card.Group>
